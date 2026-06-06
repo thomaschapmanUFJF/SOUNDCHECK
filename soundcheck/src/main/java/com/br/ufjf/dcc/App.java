671f3ec07c5bd0304667d0037cf21aae1b528df4
Thomas Chapman <thoadchapman@gmail.com>
2026-06-06 14:30:55 -0300

    feat: inicialização do projeto

---
+++ soundcheck/src/main/java/com/br/ufjf/dcc/App.java
@@ -0,0 +1,13 @@
+package com.br.ufjf.dcc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
