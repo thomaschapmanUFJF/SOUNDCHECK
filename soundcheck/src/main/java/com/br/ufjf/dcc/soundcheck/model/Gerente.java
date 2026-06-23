2fb90d25815700ca3154100c6b3c04d2ed4b7924
Gabriel <gabriel.hcgadioli@gmail.com>
2026-06-23 19:40:43 -0300

    feat: modelo de gerente

---
+++ soundcheck/src/main/java/com/br/ufjf/dcc/soundcheck/model/Gerente.java
@@ -0,0 +1,26 @@
+/*  Rafael Magno Campos da Silva - 202435012
+    Gabriel Haddad Cyrino Gadioli - 202435026
+    Thomas Adam Chapman - 202476030 */
+
+package br.ufjf.dcc.soundcheck.model;
+
+import br.ufjf.dcc.soundcheck.model.enums.TipoUsuario;
+
+public class Gerente extends Usuario {
+    private static final long serialVersionUID = 1L;
+    private String nivel;
+
+    public Gerente(String nome, String cpf, String telefone, String email, String senha, String nivel) {
+        super(nome, cpf, telefone, email, senha, TipoUsuario.GERENTE);
+        setNivel(nivel);
+    }
+
+    public String getNivel() { return nivel; }
+    public void setNivel(String nivel) {
+        if (nivel == null || nivel.trim().isEmpty()) throw new IllegalArgumentException("O nível é obrigatório.");
+        this.nivel = nivel;
+    }
+
+    @Override
+    public String toString() { return getNome() + " - Nível: " + nivel; }
+}
