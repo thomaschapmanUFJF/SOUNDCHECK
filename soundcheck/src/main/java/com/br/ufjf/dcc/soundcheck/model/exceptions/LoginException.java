3c694bda16a84a5462ab223453bfd8421bf363c1
Gabriel <gabriel.hcgadioli@gmail.com>
2026-06-23 19:40:41 -0300

    feat: exceção de autenticação

---
+++ soundcheck/src/main/java/com/br/ufjf/dcc/soundcheck/model/exceptions/LoginException.java
@@ -0,0 +1,9 @@
+/*  Rafael Magno Campos da Silva - 202435012
+    Gabriel Haddad Cyrino Gadioli - 202435026
+    Thomas Adam Chapman - 202476030 */
+
+package br.ufjf.dcc.soundcheck.model.exceptions;
+
+public class LoginException extends RuntimeException {
+    public LoginException(String message) { super(message); }
+}
