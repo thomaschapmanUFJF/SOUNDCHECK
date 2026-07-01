8a68fa41283ac32b3003500f3bc0eaedacee46b3
Thomas Chapman <thoadchapman@gmail.com>
2026-07-01 14:53:48 -0300

    feat(controller): melhorar gerenciamento de salas
    Atualizar SalaController para aprimorar funcionalidades de gerenciamento de salas.

--- src/main/java/br/ufjf/dcc/soundcheck/controller/SalaController.java
+++ src/main/java/br/ufjf/dcc/soundcheck/controller/SalaController.java
@@ -6,11 +6,18 @@ package br.ufjf.dcc.soundcheck.controller;
 
 import br.ufjf.dcc.soundcheck.model.*;
 import javax.swing.*;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class SalaController {
 
     public static void cadastrarSala(String numero, int capacidade, float valor) {
         try {
+            if (!isNumeroSalaDisponivel(numero)) {
+                JOptionPane.showMessageDialog(null,
+                    "Já existe uma sala cadastrada com este número!", "Erro", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
             Sala s = new Sala(numero, capacidade, valor);
             Persistencia.salvar(s);
             Metodos.exibeSucesso("Sala cadastrada!");
@@ -21,6 +28,11 @@ public class SalaController {
 
     public static void alterarSala(Sala s, String numero, int capacidade, float valor, boolean disponivel) {
         try {
+            if (!s.getNumero().equals(numero) && !isNumeroSalaDisponivel(numero)) {
+                JOptionPane.showMessageDialog(null,
+                    "Já existe uma sala cadastrada com este número!", "Erro", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
             Persistencia.deletar(s);
             s.setInfo(numero, capacidade, valor, disponivel);
             Persistencia.salvar(s);
@@ -33,5 +45,20 @@ public class SalaController {
     public static void deletarSala(Sala s) {
         Persistencia.deletar(s);
         Metodos.exibeSucesso("Sala removida!");
+    }
+
+    private static boolean isNumeroSalaDisponivel(String numero) {
+        try {
+            ArrayList<Sala> salas = Persistencia.carregarSalas();
+            for (Sala s : salas) {
+                if (s.getNumero().equals(numero)) {
+                    return false;
+                }
+            }
+            return true;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return true;
+        }
     }
 }
